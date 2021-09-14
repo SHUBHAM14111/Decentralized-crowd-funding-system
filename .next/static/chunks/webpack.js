ccda839edaf979c5b6a9d9e8e17a1733b0143b89
Shubham Gandhi <paskgandhi@gmail.com>
2021-09-14 22:16:15 +0530

    removed .next

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -188,7 +188,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "59fce30c0a7fe2e254fd"; }
+/******/ 		__webpack_require__.h = function() { return "f14c0744257e560062bb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
